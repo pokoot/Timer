0b70215ba88cc85b6c2459b32e3e24612cc07bdd
pokoot <pokoot@gmail.com>
2013-12-18 08:17:14 +0800

    added documentation

--- src/Timer.php
+++ src/Timer.php
@@ -2,10 +2,30 @@
 
 namespace Goldfinger;
 
+/**
+ * A utility class that computes the elapsed time.
+ *
+ * @package
+ * @version $id$
+ * @author Harold Kim
+ * @license http://pokoot.com/license.txt
+ */
 class Timer
 {
+    /**
+     * Current time value storage.
+     *
+     * @var mixed
+     * @access private
+     */
     private $time;
 
+    /**
+     * Return the current microtime.
+     *
+     * @access public
+     * @return float
+     */
     public function getCurrentTime()
     {
         list($usec, $sec) = explode(" ", microtime());
@@ -13,16 +33,35 @@ class Timer
         return ((float) $usec + (float) $sec);
     }
 
+    /**
+     * Starts the timer.
+     *
+     * @access public
+     * @return float
+     */
     public function start()
     {
         return $this->time = $this->getCurrentTime();
     }
 
+    /**
+     * Stop and resets the timer to zero.
+     *
+     * @access public
+     * @return void
+     */
     public function stop()
     {
         $this->time = 0;
     }
 
+    /**
+     * Returns the elapsed time
+     *
+     * @access public
+     * @param  int   $decimal
+     * @return float
+     */
     public function getTime($decimal = 4)
     {
         if ($this->time > 0) {
